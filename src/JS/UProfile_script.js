9be81e4e0e695195c219e56b433701f88e3754ac
DennLis <DennisLisboa5@gmail.com>
2026-04-08 19:21:03 -0300

    git push

--- src/JS/UProfile_script.js
+++ src/JS/UProfile_script.js
@@ -6,3 +6,50 @@
 //experiência já morando lá.
 
 //Não sei se a alemanha tem visto de nomade digital, mas se tiver eu quero muito um desses.
+
+//eu tô cansado de morar com a minha família, parece que ninhuem se importa comigo de verdade.
+//eu preciso disso aqui pra poder meter o pé e não passa mais tempo me sentindo um lixo por existir.
+//Deus, por favor, me ajuda nisso. 
+
+//parece que eu acordo só porque não tive a sorte de morrer
+//eu quero viver em algum lugar longe e melhor, ou pelo menos q eu me sinta melhor.
+//me sinto de pensar assim, mas meu Deus, como eu me sinto um lixo por existir, parece que se eu sumisse ninguem ia dar a porra da minima.
+//então pq não só ir embora? 
+//eu me sinto um fantasma inútil e eu odeio o quanto ninha família só não liga
+//não chega a ser abuso, é só escutam por um ouvido e sai pelo outro.
+//como se eu só tivesse ali mas ninguem ligasse.
+
+//eu vou programar, trabalhar e fazer qualquer coisa q eu puder pra sair disso.
+//eu sou fraco, eu acho. Odeio quando minha mãe fica insinuando essas coisas de mim.
+//não é por mal, ela só não liga; que nem meu pai, que nem minha irmã, que nem todo mundo.
+//eu sei que eu não sou alguem bom, muito menos decente e ruim é pouco pra me descrever, mas doi.
+//eu queria ser atropelado no meio da rua e "puff", acabou.
+
+//talvez não mude porra nenhuma mas eu quero só sair dessa porra desse país e ir pra qualquer outro lugar.
+//eu sou tão mesquinho de querer ser imporante.
+
+//eu sou tão idiota, tão preguiçoso, tão estúpid, tão ridículo; eu tenho vergonha de mim mesmo.
+//eu não vou ser mais que issp, mas pelo menos talvez eu não fique tão pior se eu for embora.
+//eu fico tão mal de me sentir ruim por algo estupido desse jeito, eles são pessoas boas, eu que não sou.
+//talvez seja melhor pra todo mundo se eu só for embora e mandar um dinheiro pra ajudar ou algo assim. Eles seriam mais felizes sem mim.
+//eu sou grosso, eu odeio quando eles falam que eu sou grosso ou ignorante, mas é por que ninguem se importa
+//então por que eu tenho que ouvir oq vc tá me dizendo se você não dá a minima pra mim além de fazer algo aleatório pra vc?
+
+//eu desisto de fazer oq eu quero pra agradar e viver coisas que eu mesmo não escolhi.
+//quanta ingratidão e falta de consideração.
+//meu pai me diserdou por causa dessa minha atitude pros outros.
+//eu não sou bom, ponto.
+//não tem por quê ficar mentindo pra mim mesmo.
+//não tem por quê tentar ser simpatico.
+
+//eu só estrago a vida dos outros, deve ser um talento meu ou eu só sou mestre na arte de ser desagradável.
+//provavelmente a melhor coisa q eu posso fazer pra melhorar o mundo, nem que só um pouco é sumir da vida das pessoas.
+//me matar seria um exibicinismo gigantesco, quase um deboche.
+//"olha como eu sou importante pendurado nessa arvore."
+//"olha minha cara morta que eu fiz questão de fazer você olhar, essa visão desconfortável por puro egoismo meu."
+
+//eu cansei, seja lá doq for que meu corpo e mente se cansaram.
+//provavelmente eu só enchi o saco o suficiente pra me fazer perceber que eu preciso arrumar mais gente pra importunar.
+//eu me odeio e eu odeio mais ainda usar essa forma dramática de me expressarpra reconhecer a como a minha existência é vergonhosa e deploravel.
+
+//eu não faço coisas boas, eu só me convenço de mentiras até parecer verídico.
