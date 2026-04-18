51fdb410fc4796ea38b3ad3daa4d59d15763065f
DennLis <DennisLisboa5@gmail.com>
2026-04-18 20:55:23 -0300

    git push

--- src/JS/Quiz_script.js
+++ src/JS/Quiz_script.js
@@ -1,2 +1,15 @@
 //eu tenho que criar um código que vai pegar as respostas do quiz e calcular a pontuação total do usuário com base nas respostas selecionadas. Depois disso, o código deve usar a pontuação total para determinar qual livro recomendar para o usuário. Eu também preciso criar uma função que vai exibir a recomendação do livro para o usuário de forma clara e atraente.
 alert("Teste de responsividade");
+
+//document.addEventListener("DOMContentLoaded", () => {
+//  const btn = document.querySelector(".start-btn");
+//  btn.addEventListener("click", () => {
+//    const paraEl = document.createElement("p");
+//    const bodyEl = document.querySelector("body");
+
+//    bodyEl.appendChild(paraEl);
+//    paraEl.textContent = "The game has started!!!";
+//  });
+//});
+
+//This sounds like a good idea 
--- src/JS/Result_script.js
+++ src/JS/Result_script.js
@@ -1,1 +1,14 @@
 alert("Teste de responsividade");
+
+//document.addEventListener("DOMContentLoaded", () => {
+//  const btn = document.querySelector(".start-btn");
+//  btn.addEventListener("click", () => {
+//    const paraEl = document.createElement("p");
+//    const bodyEl = document.querySelector("body");
+
+//    bodyEl.appendChild(paraEl);
+//    paraEl.textContent = "The game has started!!!";
+//  });
+//});
+
+//This sounds like a good idea 
